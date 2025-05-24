250514d6778650f37daa47a44e7975fcdb6b91a1
Petrone Riccardo <petroner@pascal.local>
2025-05-24 08:33:12 +0200

    aggiunto tools.h

--- AP_info/main.cpp
+++ AP_info/main.cpp
@@ -5,18 +5,9 @@
 #include <map>
 #include <set>
 #include "inserimento.h"
+#include "tools.h"
 
 using namespace std;
-
-map<corso,map<materia,vector<studente>>> universita;
-
-//controllo per evitare duplicati
-bool findName (vector<studente>  &studenti , long int matricola ){
-    for(auto studente : studenti){
-        if (studente.matr == matricola) return true;
-    }
-    return false;
-}
 
 void menu(){
     cout<<"============ GESTIONE UNIVERSITARIA ============"<<endl;
@@ -32,144 +23,6 @@ void menu(){
     cout<<"9. Salva i dati su file                        ="<<endl;
     cout<<"X. Esci                                        ="<<endl;
     cout<<"================================================"<<endl;
-}
-
-//punto 1
-string matricolaPerCorso(long int matricola){
-    string cod;
-
-    for(auto corsoMateriaStudente : universita){
-        for (auto corsoStudente : corsoMateriaStudente.second){
-            for(auto studenti : corsoStudente.second){
-                if (studenti.matr == matricola){
-                    cod = studenti.cod_corso;
-                    break;
-                }
-            }
-        }
-    }
-
-    for(auto corsoMateriaStudente : universita){
-        if (corsoMateriaStudente.first.cod_corso == cod){
-            cod = corsoMateriaStudente.first.descr_corso;
-            break;
-        }
-    }
-
-    return cod;
-}
-
-//punto 2
-string cognomePerCorso(string cogn_utente){
-    string cod;
-
-    for(auto corsoMateriaStudente : universita){
-        for(auto materiaStudente : corsoMateriaStudente.second){
-            for(auto studenti : materiaStudente.second){
-                if(cogn_utente == studenti.cogn)    cod = studenti.cod_corso;
-            }
-        }
-    }
-
-    for(auto corsoMateriaStudente : universita){
-        if(corsoMateriaStudente.first.cod_corso == cod)    cod = corsoMateriaStudente.first.descr_corso;
-    }
-
-    return cod;
-
-}
-
-//punto 3
-vector<studente> matricolaCorso(string codiceCorso){
-    vector<studente> stud;
-
-    for(auto corsoMateriaStudente : universita){
-        for (auto corsoStudente : corsoMateriaStudente.second){
-            for(auto studenti : corsoStudente.second){
-                if (studenti.cod_corso == codiceCorso and !findName(stud, studenti.matr)){
-                    stud.push_back(studenti);
-                }
-            }
-        }
-    }
-
-    return stud;
-}
-
-//punto 4
-map<vector<materia>,vector<studente>> esamiPerCorso(string codiceCorso,map<vector<materia>,vector<studente>> &esamiStudente){
-
-    for(auto corsoMateriaStudente : universita){
-        if(corsoMateriaStudente.first.cod_corso == codiceCorso){
-            for(auto materiaStudente : corsoMateriaStudente.second){
-                vector<materia> mat {materiaStudente.first};
-
-                for(auto studenti : materiaStudente.second){
-                    esamiStudente[mat].push_back(studenti);
-                }
-            }
-        }
-    }
-
-    return esamiStudente;
-}
-
-//punto 5
-int contaStudentiPerCorso (string codiceCorso,set<long int> &matPerStudenti){
-    for(auto corsoMateriaStudente : universita){
-        for(auto materiaStudente : corsoMateriaStudente.second){
-            for(auto studenti : materiaStudente.second){
-                if(codiceCorso == studenti.cod_corso){
-                    matPerStudenti.insert(studenti.matr);
-                }
-            }
-        }
-    }
-
-
-
-    return matPerStudenti.size();
-}
-
-//punto 6
-int materiePerCorso(string codiceCorso){
-
-    for(auto corsoMateriaStudente : universita){
-            return corsoMateriaStudente.second.size();
-    }
-}
-
-//punto 7
-map<string,string> descrPerMateria(string descr,map<string,string> &descrMateria){
-
-    transform(descr.begin(),descr.end(),descr.begin(),::tolower);
-    for(auto corsoMateriaStudente : universita){
-        for(auto materiaStudente : corsoMateriaStudente.second){
-            //converto la parola in minuscolo
-            string materia = materiaStudente.first.descr_materia;
-            transform(materia.begin(),materia.end(),materia.begin(),::tolower);
-
-            if(materia.find(descr) != string::npos){
-
-                descrMateria[materiaStudente.first.descr_materia] = materiaStudente.first.cod_materia;
-            }
-        }
-    }
-    return descrMateria;
-}
-
-//punto 8
-void inserimentoConMateria(long int matr,string cogn,string nome,string materia){
-    for(auto &corsoMateriaStudente : universita){
-        for(auto& materiaStudente : corsoMateriaStudente.second){
-            if(materiaStudente.first.cod_materia == materia){
-                studente ins{matr,cogn,nome,corsoMateriaStudente.first.cod_corso};
-
-                materiaStudente.second.push_back(ins);
-
-            }
-        }
-    }
 }
 
 int main()
@@ -297,6 +150,8 @@ int main()
                 }
                 descrMateria.clear();
 
+                break;
+
             case '8':
 
                 cout<<"Inserisci il codice della materia che frequenti: ";
@@ -326,18 +181,17 @@ int main()
                         }
                     }
                 }
+
                 break;
 
                 fout.close();
 
 
-
-
-
-
         }
         menu();
+        cout<<"Fai la tua scelta: ";
         cin>>scelta;
+
 
     }
 
---
+++ AP_info/tools.h
@@ -0,0 +1,156 @@
+#ifndef TOOLS_H_INCLUDED
+#define TOOLS_H_INCLUDED
+#include <string>
+#include <vector>
+#include <map>
+
+map<corso,map<materia,vector<studente>>> universita;
+
+//controllo per evitare duplicati
+bool findName (vector<studente>  &studenti , long int matricola ){
+    for(auto studente : studenti){
+        if (studente.matr == matricola) return true;
+    }
+    return false;
+}
+
+//punto 1
+string matricolaPerCorso(long int matricola){
+    string cod;
+
+    for(auto corsoMateriaStudente : universita){
+        for (auto corsoStudente : corsoMateriaStudente.second){
+            for(auto studenti : corsoStudente.second){
+                if (studenti.matr == matricola){
+                    cod = studenti.cod_corso;
+                    break;
+                }
+            }
+        }
+    }
+
+    for(auto corsoMateriaStudente : universita){
+        if (corsoMateriaStudente.first.cod_corso == cod){
+            cod = corsoMateriaStudente.first.descr_corso;
+            break;
+        }
+    }
+
+    return cod;
+}
+
+//punto 2
+string cognomePerCorso(string cogn_utente){
+    string cod;
+
+    for(auto corsoMateriaStudente : universita){
+        for(auto materiaStudente : corsoMateriaStudente.second){
+            for(auto studenti : materiaStudente.second){
+                if(cogn_utente == studenti.cogn)    cod = studenti.cod_corso;
+            }
+        }
+    }
+
+    for(auto corsoMateriaStudente : universita){
+        if(corsoMateriaStudente.first.cod_corso == cod)    cod = corsoMateriaStudente.first.descr_corso;
+    }
+
+    return cod;
+
+}
+
+//punto 3
+vector<studente> matricolaCorso(string codiceCorso){
+    vector<studente> stud;
+
+    for(auto corsoMateriaStudente : universita){
+        for (auto corsoStudente : corsoMateriaStudente.second){
+            for(auto studenti : corsoStudente.second){
+                if (studenti.cod_corso == codiceCorso and !findName(stud, studenti.matr)){
+                    stud.push_back(studenti);
+                }
+            }
+        }
+    }
+
+    return stud;
+}
+
+//punto 4
+map<vector<materia>,vector<studente>> esamiPerCorso(string codiceCorso,map<vector<materia>,vector<studente>> &esamiStudente){
+
+    for(auto corsoMateriaStudente : universita){
+        if(corsoMateriaStudente.first.cod_corso == codiceCorso){
+            for(auto materiaStudente : corsoMateriaStudente.second){
+                vector<materia> mat {materiaStudente.first};
+
+                for(auto studenti : materiaStudente.second){
+                    esamiStudente[mat].push_back(studenti);
+                }
+            }
+        }
+    }
+
+    return esamiStudente;
+}
+
+//punto 5
+int contaStudentiPerCorso (string codiceCorso,set<long int> &matPerStudenti){
+    for(auto corsoMateriaStudente : universita){
+        for(auto materiaStudente : corsoMateriaStudente.second){
+            for(auto studenti : materiaStudente.second){
+                if(codiceCorso == studenti.cod_corso){
+                    matPerStudenti.insert(studenti.matr);
+                }
+            }
+        }
+    }
+
+
+
+    return matPerStudenti.size();
+}
+
+//punto 6
+int materiePerCorso(string codiceCorso){
+
+    for(auto corsoMateriaStudente : universita){
+            return corsoMateriaStudente.second.size();
+    }
+}
+
+//punto 7
+map<string,string> descrPerMateria(string descr,map<string,string> &descrMateria){
+
+    transform(descr.begin(),descr.end(),descr.begin(),::tolower);
+
+    for(auto corsoMateriaStudente : universita){
+        for(auto materiaStudente : corsoMateriaStudente.second){
+            //converto la parola in minuscolo
+            string materia = materiaStudente.first.descr_materia;
+            transform(materia.begin(),materia.end(),materia.begin(),::tolower);
+
+            if(materia.find(descr) != string::npos){
+
+                descrMateria[materiaStudente.first.descr_materia] = materiaStudente.first.cod_materia;
+            }
+        }
+    }
+    return descrMateria;
+}
+
+//punto 8
+void inserimentoConMateria(long int matr,string cogn,string nome,string materia){
+    for(auto &corsoMateriaStudente : universita){
+        for(auto& materiaStudente : corsoMateriaStudente.second){
+            if(materiaStudente.first.cod_materia == materia){
+                studente ins{matr,cogn,nome,corsoMateriaStudente.first.cod_corso};
+
+                materiaStudente.second.push_back(ins);
+
+            }
+        }
+    }
+}
+
+#endif // TOOLS_H_INCLUDED
